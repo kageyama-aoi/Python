7ee5c3e085aa3e2da3c134483c070554616125f9
kageyama-aoi <kisaragisouma09@gmail.com>
2025-12-24 10:41:59 +0900

    各種ファイル追加

---
+++ 00_画面操作自動化/run_validation.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+REM エラーログのパスを指定 (新しいlogsディレクトリ内を指すように変更)
+set LOG_DIR=logs\batch_error_logs
+set LOG_FILE=%LOG_DIR%\validation_error_%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%.log
+
+REM ログディレクトリが存在しない場合は作成
+if not exist "%LOG_DIR%" mkdir "%LOG_DIR%"
+
+REM Pythonスクリプトのパスを変更
+python src\create_task_report.py kensho 2>> %LOG_FILE%
+@REM python 00_MAIN\create_task_report.py kensho 2>> %LOG_FILE%
+
+endlocal
+pause
