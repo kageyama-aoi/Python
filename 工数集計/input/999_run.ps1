d466610f2c1d793d026914520dd6caca1ed1aee2
kageyama-aoi <kisaragisouma09@gmail.com>
2025-12-25 17:08:25 +0900

    起動方法修正(bat→powershell)

---
+++ 工数集計/input/999_run.ps1
@@ -0,0 +1,14 @@
+﻿Set-Location $PSScriptRoot
+
+$datetime = Get-Date -Format "yyyyMMdd_HHmmss"
+$filename = "error_$datetime.txt"
+
+Set-Location "C:\Users\kageyama\Tools\Python\工数集計"
+
+$logPath = Join-Path "error_log" $filename
+
+python main.py 2> $logPath
+
+Write-Host "処理完了"
+Write-Host "エラーログ: $logPath"
+Pause
